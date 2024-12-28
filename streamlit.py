6b044065dc8b83eb0d54eae5df73da0b0ddbb6cf
Alex Pandya <alex.pandya05@gmail.com>
2024-12-27 20:50:11 -0600

    add text to streamlit site

--- streamlit.py
+++ streamlit.py
@@ -25,7 +25,8 @@ def visualize_data(year, x, y):
     
     fig = px.scatter(df.loc[df.SEASON_START==year], x=x, y=y, color="Retained",
                     hover_name="NAME", hover_data=["TEAMS_LIST"],
-                    color_discrete_map={"Yes":"dodgerblue", "No":"lightcoral"})
+                    color_discrete_map={"Yes":"cornflowerblue", "No":"lightcoral"},
+                    labels={"Retained":"Retained?"})
     st.plotly_chart(fig)
 
     return None
@@ -74,19 +75,38 @@ preds_exp = preds.explode("TEAMS_AS_LIST", ignore_index=True)
 teams = preds_exp.TEAMS_AS_LIST.unique()
 teams.sort()
 
-#-------------------------Generate output--------------------------------------#
+#---------------Visualizing raw data-------------------------------------------#
 
-st.title('Predicting NBA Transactions')
+st.title('Predicting NBA Player Retention')
+
+st.markdown('''This page provides an interactive executive summary of the
+            project _Predicting NBA Player Retention_, completed by Alex Pandya,
+            Peter Johnson, Andrew Newman, Ryan Moruzzi, and Collin Litterell.
+            It was named a Top Project for the Erdos Institute Data Science
+            Bootcamp, Fall 2024.''')
+
+st.markdown('''The project aims to answer the following question: _can we
+            predict whether or not a given NBA player will still be in the
+            league next season, given just present-season statistics and
+            transaction data?_''')
 
 st.subheader("Visualizing the data")
+
+st.markdown('''The scatterplot below provides a visualization for the raw data,
+               which includes counting stats, advanced stats, and salary data
+               for seasons between 1990 and 2023.  The scatterplot shows
+               whether or not the player was _retained_ (whether or not they
+               played at least one game in the following season) in color.''')
+
 
 features = df.select_dtypes(include='number').columns.drop(['PLAYER_ID', 
                                             'SEASON_START', 'IN_LEAGUE_NEXT'])
 
-stats = features.drop(['WAIVED', 'RELEASED', 'TRADED',        
+stats = list(features.drop(['WAIVED', 'RELEASED', 'TRADED',        
        'WAIVED_OFF', 'WAIVED_REG', 'WAIVED_POST', 'RELEASED_OFF',               
        'RELEASED_REG', 'RELEASED_POST', 'TRADED_OFF', 'TRADED_REG',             
-       'TRADED_POST'])
+       'TRADED_POST']))
+stats.sort()
 
 x_stat = st.selectbox("x-axis:", stats)
 y_stat = st.selectbox("y-axis:", stats)
@@ -98,8 +118,20 @@ visualize_data(year, x_stat, y_stat)
 
 st.subheader("Model predictions")
 
+st.markdown('''The bar charts below summarize the predictions of our
+               best-performing model (XGBoost with SMOTE-augmented training
+               data, calibrated using Platt's method) trained on data from
+               seasons before the one set in the slider.  The height of the bar
+               gives the probability that the player will play at any point
+               in the next season, and the color gives whether or not they did
+               play in the following season.''')
+
 pred_year = st.slider(label="Season start year:", min_value=2017,
                       max_value=2023) 
 pred_team = st.selectbox("Team:", teams)
 
 visualize_preds(pred_year, pred_team)
+
+st.markdown('''Note: for the 2023 season we do not yet know whether a given
+               player will play _at any point_, so the color of the bars comes
+               from the classifier.''')
